1bf76f317449fb5585fd549b951264e7a1b13041
Havindu Piyumal Dissanayake <havindu1992@gmail.com>
2024-05-27 14:43:07 +1000

    implement route changes to home page.

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   MenuContainer,
   BackgroundImage,
   MenuBodyContainer,
 } from "./menu-item.styles.jsx";
 
-export const MenuItem = ({ category: { title, imageUrl } }) => {
+export const MenuItem = ({ category: { title, imageUrl, route } }) => {
+  const naviagte = useNavigate();
+
+  const onNavigateHandler = () => naviagte(route);
+
   return (
-    <MenuContainer>
+    <MenuContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <MenuBodyContainer>
-        <Link to={`/shop/${title}`}>
+        <Link to={route}>
           <h2>{title}</h2>
         </Link>
         <p>Shop Now</p>
